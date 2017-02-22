cb67c518619121ab2b555b0069334fee1b48c42c
Michael Bryzek <mbryzek@alum.mit.edu>
2017-02-22 15:46:16 -0500

    Update

--- release.go
+++ release.go
@@ -8,7 +8,14 @@ func main() {
 	executor := executor.Create("lib-reference-scala")
 	executor = executor.Add("git checkout master")
 	executor = executor.Add("git pull --rebase")
+	executor = executor.Add("sbt test")
+
+	executor = executor.Add("apidoc update")
+	executor = executor.Add("go run script/generate.go")
+	executor = executor.Add("echo version := \\\"`sem-info tag next`\\\" > VERSION.sbt")
+	executor = executor.Add("git commit -a -m 'Auto create version `sem-info tag next`'")
 	executor = executor.Add("dev tag")
+
 	executor = executor.Add("sbt publish")
 	executor.Run()
 }
