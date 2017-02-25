cc9024af770711d09b0e7cb65381b6451f591d3a
Michael Bryzek <mbryzek@alum.mit.edu>
2017-02-25 10:32:27 -0500

    Run tests after update

--- release.go
+++ release.go
@@ -8,11 +8,12 @@ func main() {
 	executor := executor.Create("lib-reference-scala")
 	executor = executor.Add("git checkout master")
 	executor = executor.Add("git pull --rebase")
-	executor = executor.Add("sbt test")
 
 	executor = executor.Add("apidoc update")
 	executor = executor.Add("go get -u github.com/flowcommerce/json-reference/common")
 	executor = executor.Add("go run script/generate.go")
+	executor = executor.Add("sbt test")
+
 	executor = executor.Add("echo version := \\\"`sem-info tag next`\\\" > VERSION.sbt")
 	executor = executor.Add("git commit -a -m 'Auto create version `sem-info tag next`'")
 	executor = executor.Add("dev tag")
