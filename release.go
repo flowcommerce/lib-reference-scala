35d67c2f5e4bbcdd6c251de0de4c0ebbc5b7a5af
Michael Bryzek <mbryzek@flow.io>
2018-01-24 11:32:30 -0500

    Upgrade release script to use scala_library. Add play26 branch

--- release.go
+++ release.go
@@ -1,23 +1,9 @@
 package main
 
 import (
-	"github.com/flowcommerce/tools/executor"
+	"github.com/flowcommerce/tools/scala_library"
 )
 
 func main() {
-	executor := executor.Create("lib-reference-scala")
-	executor = executor.Add("git checkout master")
-	executor = executor.Add("git pull --rebase")
-
-	executor = executor.Add("apibuilder update")
-	executor = executor.Add("go get -u github.com/flowcommerce/json-reference/common")
-	executor = executor.Add("go run script/generate.go")
-	executor = executor.Add("sbt test")
-
-	executor = executor.Add("echo version := \\\"`sem-info tag next`\\\" > VERSION.sbt")
-	executor = executor.Add("git commit -a -m 'Auto create version `sem-info tag next`'")
-	executor = executor.Add("dev tag")
-
-	executor = executor.Add("sbt +publish")
-	executor.Run()
+	scala_library.Run()
 }
