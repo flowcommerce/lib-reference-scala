ee8578ee1bebe813581999326fd842c4def414b7
Paolo Lim <paolo@flow.io>
2018-02-21 13:37:34 -0500

    Add carrier and services helper classes and tests

---
+++ src/main/scala/io/flow/reference/Carriers.scala
@@ -0,0 +1,19 @@
+package io.flow.reference
+
+import io.flow.reference.v0.models.Carrier
+
+object Carriers extends Validation[Carrier] {
+  override val cache: Map[String, Carrier] = Map(
+    data.Carriers.all.flatMap { t =>
+      Seq(
+        t.id.toLowerCase -> t,
+        t.name.toLowerCase -> t,
+        t.name.replaceAll("[/_]", " ").toLowerCase -> t
+      )
+    }: _*
+  )
+
+  override def singular = "carrier"
+  override def plural = "carriers"
+  override def name(c: Carrier) = c.name
+}
---
+++ src/test/scala/io/flow/reference/CarrierServicesSpec.scala
@@ -0,0 +1,49 @@
+package io.flow.reference
+
+import org.scalatest.{FunSpec, Matchers}
+
+class CarrierServicesSpec extends FunSpec with Matchers {
+
+  it("be unique") {
+    data.CarrierServices.all.groupBy(_.id).filter { _._2.size > 1 }.keys should be(Set())
+  }
+
+  it("be sorted") {
+    data.CarrierServices.all.map(_.id) should be(data.CarrierServices.all.map(_.id).sortBy { _.toLowerCase })
+  }
+
+  it("have no blanks") {
+    data.CarrierServices.all.find(_.id.trim.isEmpty) should be(None)
+  }
+
+  it("find") {
+    data.CarrierServices.all.foreach { carrierService =>
+      // find by name
+      CarrierServices.find(carrierService.name).getOrElse {
+        sys.error(s"${carrierService.name} missing")
+      }
+
+      // find by id
+      CarrierServices.find(carrierService.id).getOrElse {
+        sys.error(s"${carrierService.id} missing")
+      }
+    }
+
+    CarrierServices.find("other") should be(None)
+  }
+
+  it("mustFind") {
+    data.CarrierServices.all.foreach { carrierService =>
+      // must find by name
+      CarrierServices.mustFind(carrierService.name).name.toLowerCase should be(carrierService.name.trim.toLowerCase)
+
+      // must find by id
+      CarrierServices.mustFind(carrierService.id).id.toLowerCase should be(carrierService.id.trim.toLowerCase)
+    }
+
+    intercept[Throwable] {
+      CarrierServices.mustFind("other")
+    }.getMessage should be("The following carrier service is invalid: [other]. See https://api.flow.io/reference/carrier-services for a list of all valid carrier-services.")
+  }
+
+}
