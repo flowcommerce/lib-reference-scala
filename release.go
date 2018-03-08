46d8769e519b53996bb07f6830e26b27a0a087b7
Mike Roth <roth@flow.io>
2018-03-07 20:57:53 -0500

    Generate reference data (#28)

--- release.go
+++ release.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"github.com/flowcommerce/tools/executor"
 	"github.com/flowcommerce/tools/scala_library"
 )
 
 func main() {
+	executor := executor.Create("lib-reference-scala-json")
+	executor = executor.Add("go get -u github.com/flowcommerce/json-reference/common")
+	executor = executor.Add("go run script/generate.go")
+	executor.Run()
+
 	scala_library.Run()
 }
