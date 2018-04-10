b7307fab3e02799e1e7a014e6e14a2d2f33d79cc
Paolo Lim <paolo@flow.io>
2018-04-10 17:33:16 -0400

    Add localized translations to provinces (#33)
    * gofmt
    
    * wip
    
    * save
    
    * finalize models

--- script/generate.go
+++ script/generate.go
@@ -14,15 +14,15 @@ import (
 	"github.com/flowcommerce/tools/util"
 	"os"
 	"regexp"
-	"strings"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
 const Directory = "src/main/scala/io/flow/reference/data"
 
 type Instance struct {
-	Name string
+	Name  string
 	Value string
 }
 
@@ -38,13 +38,13 @@ func writeFile(objectName string, imports string, instances []Instance) {
 	writer.WriteString(fmt.Sprintf("import io.flow.reference.v0.models.%s\n\n", imports))
 	writer.WriteString(fmt.Sprintf("object %s {\n\n", objectName))
 
-	for _, instance := range(instances) {
+	for _, instance := range instances {
 		writer.WriteString(fmt.Sprintf("  val %s = %s\n", scalaName(instance.Name), instance.Value))
 	}
 	writer.WriteString("\n")
 
 	writer.WriteString("  val all = Seq(\n    ")
-	for i, instance := range(instances) {
+	for i, instance := range instances {
 		if i > 0 {
 			writer.WriteString(",\n    ")
 		}
@@ -74,7 +74,7 @@ func processCarriers() {
 	instances := []Instance{}
 	for _, c := range common.Carriers() {
 		instances = append(instances, Instance{
-			Name: strings.Replace(c.Id, "-", "_", -1),
+			Name:  strings.Replace(c.Id, "-", "_", -1),
 			Value: fmt.Sprintf("Carrier(id = \"%s\", name = \"%s\", trackingUrl = \"%s\")", c.Id, c.Name, c.TrackingUrl),
 		})
 	}
@@ -86,7 +86,7 @@ func processCarrierServices() {
 	instances := []Instance{}
 	for _, cs := range common.CarrierServices() {
 		instances = append(instances, Instance{
-			Name: strings.Replace(cs.Id, "-", "_", -1),
+			Name:  strings.Replace(cs.Id, "-", "_", -1),
 			Value: fmt.Sprintf("CarrierService(id = \"%s\", name = \"%s\", carrier = io.flow.reference.v0.models.Carrier(id = \"%s\", name = \"%s\", trackingUrl = \"%s\"))", cs.Id, cs.Name, cs.Carrier.Id, cs.Carrier.Name, cs.Carrier.TrackingUrl),
 		})
 	}
@@ -97,9 +97,21 @@ func processCarrierServices() {
 func processProvinces() {
 	instances := []Instance{}
 	for _, p := range common.Provinces() {
+		// translations is a list of locales with their translated names
+		var localizedTranslations []string
+		for _, t := range p.Translations {
+			l := t.Locale
+			localizedTranslations = append(
+				localizedTranslations,
+				fmt.Sprintf("io.flow.reference.v0.models.LocalizedTranslation(locale = io.flow.reference.v0.models.Locale(id = \"%s\", name = \"%s\", country = \"%s\", language = \"%s\", numbers = io.flow.reference.v0.models.LocaleNumbers(decimal = \"%s\", group = \"%s\")), name = \"%s\")", l.Id, l.Name, l.Country, l.Language, l.Numbers.Decimal, l.Numbers.Group, t.Name),
+			)
+		}
+		translations := strings.Join(localizedTranslations, ", ")
+
+		// now create the province and simply add the locale translations
 		instances = append(instances, Instance{
-			Name: strings.Replace(p.Id, "-", "_", -1),
-			Value: fmt.Sprintf("Province(id = \"%s\", iso31662 = \"%s\", name = \"%s\", country = \"%s\", provinceType = io.flow.reference.v0.models.ProvinceType(\"%s\"))", p.Id, p.Iso_3166_2, p.Name, p.Country, p.ProvinceType),
+			Name:  strings.Replace(p.Id, "-", "_", -1),
+			Value: fmt.Sprintf("Province(id = \"%s\", iso31662 = \"%s\", name = \"%s\", country = \"%s\", provinceType = io.flow.reference.v0.models.ProvinceType(\"%s\"), translations = Some(Seq(%s)))", p.Id, p.Iso_3166_2, p.Name, p.Country, p.ProvinceType, translations),
 		})
 	}
 
@@ -119,7 +131,7 @@ func processCountries() {
 		}
 
 		instances = append(instances, Instance{
-			Name: c.Iso_3166_3,
+			Name:  c.Iso_3166_3,
 			Value: fmt.Sprintf("Country(iso31662 = \"%s\", iso31663 = \"%s\", defaultCurrency = %s, measurementSystem = \"%s\", name = \"%s\", languages = %s, timezones = %s)", c.Iso_3166_2, c.Iso_3166_3, defaultCurrency, measurementSystem, c.Name, scalaArrayQuoted(c.Languages), scalaArrayQuoted(c.Timezones)),
 		})
 	}
@@ -127,17 +139,16 @@ func processCountries() {
 	writeFile("Countries", "Country", instances)
 }
 
-
 func processRegions() {
 	instances := []Instance{}
 	for _, r := range common.Regions() {
 		measurementSystems := []string{}
-		for _, system := range(r.MeasurementSystems) {
+		for _, system := range r.MeasurementSystems {
 			measurementSystems = append(measurementSystems, system)
 		}
 
 		instances = append(instances, Instance{
-			Name: r.Name,
+			Name:  r.Name,
 			Value: fmt.Sprintf("Region(id = \"%s\", name = \"%s\", countries = %s, languages = %s, currencies = %s, measurementSystems = %s, timezones = %s)", r.Id, r.Name, scalaArrayQuoted(r.Countries), scalaArrayQuoted(r.Languages), scalaArrayQuoted(r.Currencies), scalaArrayQuoted(measurementSystems), scalaArrayQuoted(r.Timezones)),
 		})
 	}
@@ -163,7 +174,7 @@ func processCurrencies() {
 		}
 
 		instances = append(instances, Instance{
-			Name: c.Iso_4217_3,
+			Name:  c.Iso_4217_3,
 			Value: fmt.Sprintf("Currency(iso42173 = \"%s\", name = \"%s\", numberDecimals = %v, defaultLocale = %s, symbols = %s)", c.Iso_4217_3, c.Name, c.NumberDecimals, defaultLocale, symbols),
 		})
 	}
@@ -175,7 +186,7 @@ func processLanguages() {
 	instances := []Instance{}
 	for _, l := range common.Languages() {
 		instances = append(instances, Instance{
-			Name: l.Iso_639_2,
+			Name:  l.Iso_639_2,
 			Value: fmt.Sprintf("Language(iso6392 = \"%s\", name = \"%s\")", l.Iso_639_2, l.Name),
 		})
 	}
@@ -187,7 +198,7 @@ func processLocales() {
 	instances := []Instance{}
 	for _, l := range common.Locales() {
 		instances = append(instances, Instance{
-			Name: formatLocaleIdForName(l.Id),
+			Name:  formatLocaleIdForName(l.Id),
 			Value: fmt.Sprintf("Locale(id = \"%s\", name = \"%s\", country = \"%s\", language = \"%s\", numbers = LocaleNumbers(decimal = \"%s\", group = \"%s\"))", l.Id, l.Name, l.Country, l.Language, l.Numbers.Decimal, l.Numbers.Group),
 		})
 	}
@@ -199,7 +210,7 @@ func processTimezones() {
 	instances := []Instance{}
 	for _, t := range common.Timezones() {
 		instances = append(instances, Instance{
-			Name: t.Name,
+			Name:  t.Name,
 			Value: fmt.Sprintf("Timezone(name = \"%s\", offset = %d, description = \"%s\")", t.Name, t.Offset, t.Description),
 		})
 	}
@@ -226,7 +237,7 @@ func processPaymentMethods() {
 		images := fmt.Sprintf("PaymentMethodImages(%s  small = %s,%s  medium = %s,%s  large = %s%s)", divider, toImage(pm.Images.Small), divider, toImage(pm.Images.Medium), divider, toImage(pm.Images.Large), divider)
 
 		instances = append(instances, Instance{
-			Name: pm.Id,
+			Name:  pm.Id,
 			Value: fmt.Sprintf("PaymentMethod(id = \"%s\", `type` = %s, name = \"%s\", images = %s, regions = %s)", pm.Id, typ, pm.Name, images, scalaArrayQuoted(pm.Regions)),
 		})
 	}
@@ -240,31 +251,31 @@ func toImage(i common.PaymentMethodImage) string {
 
 func scalaArrayQuoted(values []string) string {
 	value := ""
-	for i, v := range(values) {
+	for i, v := range values {
 		if i > 0 {
-			value = value + ", ";
+			value = value + ", "
 		}
 		value = value + fmt.Sprintf("\"%s\"", v)
 	}
 	if value == "" {
 		return "Nil"
 	} else {
-		return "Seq(" + value + ")";
+		return "Seq(" + value + ")"
 	}
 }
 
 func scalaArrayUnquoted(values []string) string {
 	value := ""
-	for i, v := range(values) {
+	for i, v := range values {
 		if i > 0 {
-			value = value + ", ";
+			value = value + ", "
 		}
 		value = value + v
 	}
 	if value == "" {
 		return "Nil"
 	} else {
-		return "Seq(" + value + ")";
+		return "Seq(" + value + ")"
 	}
 }
 
@@ -283,7 +294,7 @@ func scalaName(name string) string {
 	safe = regexp.MustCompile("[^A-Za-z0-9Åçôãéí]+").ReplaceAllString(safe, "_")
 
 	s := ""
-	for _, el := range(strings.Split(safe, "_")) {
+	for _, el := range strings.Split(safe, "_") {
 		s = s + UpcaseInitial(strings.ToLower(el))
 	}
 	return s
@@ -294,8 +305,8 @@ func formatLocaleIdForName(id string) string {
 }
 
 func UpcaseInitial(str string) string {
-    for pos, char := range str {
-        return string(unicode.ToUpper(char)) + string([]rune(str)[pos+1:])
-    }
-    return ""
+	for pos, char := range str {
+		return string(unicode.ToUpper(char)) + string([]rune(str)[pos+1:])
+	}
+	return ""
 }
