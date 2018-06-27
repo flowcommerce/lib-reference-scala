f608d0ecdfce2c0d6686be5f7236feb8723b6c09
Michael Bryzek <mike@flow.io>
2018-06-27 16:47:07 -0400

    Improve spec

--- src/test/scala/io/flow/reference/functions/RegionCoverageSpec.scala
+++ src/test/scala/io/flow/reference/functions/RegionCoverageSpec.scala
@@ -7,9 +7,9 @@ import io.flow.reference.data
 class RegionCoverageSpec extends FunSpec with Matchers {
 
   it("encompassingRegions should include a test region and any region that includes it") {
-    data.Regions.France.encompassingRegions should be(Seq(data.Regions.France, data.Regions.EuropeanUnion, data.Regions.Eurozone, data.Regions.EuropeanEconomicArea, data.Regions.Europe, data.Regions.World))
-    data.Regions.EuropeanUnion.encompassingRegions should be(Seq(data.Regions.EuropeanUnion, data.Regions.EuropeanEconomicArea, data.Regions.Europe, data.Regions.World))
-    data.Regions.World.encompassingRegions should be(Seq(data.Regions.World))
+    data.Regions.France.encompassingRegions.map(_.id).sorted should be(Seq(data.Regions.France, data.Regions.EuropeanUnion, data.Regions.Eurozone, data.Regions.EuropeanEconomicArea, data.Regions.Europe, data.Regions.World).map(_.id).sorted)
+    data.Regions.EuropeanUnion.encompassingRegions.map(_.id) should be(Seq(data.Regions.EuropeanUnion, data.Regions.EuropeanEconomicArea, data.Regions.Europe, data.Regions.World).map(_.id))
+    data.Regions.World.encompassingRegions.map(_.id) should be(Seq(data.Regions.World.id))
   }
 
   it("enclosedRegions should include a test region and all regions it contains") {
