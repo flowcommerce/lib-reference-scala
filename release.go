10b6384596771361277e22c3c0f07305cb9d85fb
Jean Logeart <jean.logeart@gmail.com>
2020-01-06 22:21:20 -0500

    Cross test in release script

--- release.go
+++ release.go
@@ -9,7 +9,7 @@ func main() {
 	executor := executor.Create("lib-reference-scala-json")
 	executor = executor.Add("go get -u github.com/flowcommerce/json-reference/common")
 	executor = executor.Add("go run script/generate.go")
-	executor = executor.Add("sbt test")
+	executor = executor.Add("sbt +test")
 	executor = executor.Add("git commit --allow-empty -am 'autocommit: Regenerate models'")
 
 	executor.Run()
