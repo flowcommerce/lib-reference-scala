fe58b652a92d09aaea6cf6bceeeedcc89b995bed
Michael Bryzek <mbryzek@alum.mit.edu>
2024-01-07 17:30:30 -0500

    wip

--- src/main/scala/io/flow/reference/valid/Validation.scala
+++ src/main/scala/io/flow/reference/valid/Validation.scala
@@ -4,40 +4,50 @@ import cats.data.ValidatedNec
 import cats.implicits._
 import io.flow.reference.Reference
 
+/*
+ * @param prefix
+ *   A prefix to place before the singular or plural of `T`, such as "destination" to yield "destination country" or
+ *   "destination countries" if `T` is `Country`.
+ * @param suffix
+ *   A suffix to place after the singular or plural of `T`, such as "of origin" to yield "country of origin" or
+ *   "countries of origin" if `T` is `Country`.
+ */
+case class Grammar(
+  prefix: Option[String],
+  suffix: Option[String],
+)
+object Grammar {
+  val None: Grammar = Grammar(prefix = None, suffix = None)
+}
+
 /** A trait that allows a type `T` to be validated as an Either
   */
 trait Validation[T] extends Reference[T] {
 
-  def validateSingle(
+  def validate(
     id: String,
-    prefix: Option[String] = None,
-    suffix: Option[String] = None,
+  )(implicit
+    grammar: Grammar = Grammar.None,
   ): ValidatedNec[String, T] = {
     val trimmed = id.trim
     find(trimmed).toValidNec(
-      singleInvalid(trimmed, prefix.getOrElse(""), suffix.getOrElse("")),
+      singleInvalid(trimmed, prefix = grammar.prefix.getOrElse(""), suffix = grammar.suffix.getOrElse("")),
     )
   }
 
   /** Validates a list of ids for the given type `T`.
     * @param ids
     *   The ids to validate.
-    * @param prefix
-    *   A prefix to place before the singular or plural of `T`, such as "destination" to yield "destination country" or
-    *   "destination countries" if `T` is `Country`.
-    * @param suffix
-    *   A suffix to place after the singular or plural of `T`, such as "of origin" to yield "country of origin" or
-    *   "countries of origin" if `T` is `Country`.
     */
   def validate(
     ids: Seq[String],
-    prefix: Option[String] = None,
-    suffix: Option[String] = None,
+  )(implicit
+    grammar: Grammar = Grammar.None,
   ): ValidatedNec[String, Seq[T]] = {
     val distinctTrimmedIds = ids.map(_.trim).distinct
-    val strictlyInvalidIds = distinctTrimmedIds.filterNot { id => validateSingle(id).isValid }
+    val strictlyInvalidIds = distinctTrimmedIds.filterNot { id => validate(id).isValid }
 
-    invalidError(strictlyInvalidIds, prefix.getOrElse(""), suffix.getOrElse("")) match {
+    invalidError(strictlyInvalidIds, grammar.prefix.getOrElse(""), grammar.suffix.getOrElse("")) match {
       case Nil => distinctTrimmedIds.map(mustFind).validNec
       case errors => errors.map(_.invalidNec).sequence
     }
