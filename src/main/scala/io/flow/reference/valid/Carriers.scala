7466749d983adfbc2ddb7c4c32c3e13463c25495
Michael Bryzek <mbryzek@alum.mit.edu>
2024-01-07 17:16:11 -0500

    wip

--- src/main/scala/io/flow/reference/valid/Carriers.scala
+++ src/main/scala/io/flow/reference/valid/Carriers.scala
@@ -1,6 +1,7 @@
 package io.flow.reference.valid
 
-import io.flow.reference.BaseCarriers
-import io.flow.reference.v0.models.Carrier
+import io.flow.reference._
+import io.flow.reference.v0.models._
 
 object Carriers extends BaseCarriers with Validation[Carrier]
+object CarrierServices extends BaseCarrierServices with Validation[CarrierService]
